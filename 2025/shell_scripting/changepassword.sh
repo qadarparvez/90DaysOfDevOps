1aeab84f43282388fe33f084254617288383e105
Ubuntu <ubuntu@ip-172-31-15-4.ap-south-1.compute.internal>
2025-03-07 21:18:08 +0000

    change password

---
+++ 2025/shell_scripting/changepassword.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#this script is to change the password
+
+read -p "enter the username: " username
+
+count=$(cat /etc/passwd | grep $username | wc | awk '{print $1}')
+
+if [ $count == 1 ];
+then
+	read -s -p "enter new password: " password
+	echo -e "$username:$password" | sudo chpasswd
+	echo "password for $username reset successfully"
+else
+	echo "$username does not exist"
+fi
